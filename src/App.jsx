65a8dc997d08cb5bc60ce7c9052a7bae3e91f583
wz <wenzhe1423@gmail.com>
2026-01-25 14:21:09 +0800

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import "./App.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 function App() {
+  // Setting Hero Header
+  const headers = [
+    "Student Developer.",
+    "Web and Mobile Development.",
+    "Part-Time Freelancer.",
+  ];
+  
   return (
-    <main className="bg-zinc-900 w-full min-h-full zalando-sans  text-white">
+    <main className="bg-zinc-900 w-full min-h-full zalando-sans tracking-tight  text-white">
       {/* Nav */}
-      <nav className="w-full p-8 px-50 flex justify-between text-2xl tracking-tight absolute top-0">
+      <nav className="w-full p-8 px-50 flex justify-between text-2xl absolute top-0">
         <p className="text-3xl font-semibold">Tan Wen Zhe</p>
         <ul className="flex gap-4 font-light">
           <li>About</li>
@@ -19,11 +26,15 @@ function App() {
       <div className="p-2 w-full min-h-screen">
         {/* Hero */}
         <div className="w-full items-center justify-center flex flex-col h-screen">
-          <p className="text-8xl font-semibold">I Am a Student Developer.</p>
+          <ul className="text-8xl flex text-center justify-center font-semibold h-24 w-full relative">
+            <li className="heroCaption absolute">{headers[0]}</li>
+            <li className="heroCaption absolute">{headers[1]}</li>
+            <li className="heroCaption absolute">{headers[2]}</li>
+          </ul>
           <p className="leading-20 text-4xl text-zinc-500">Hi, I am Wen Zhe.</p>
-          <button className="flex items-center gap-4 mt-2 rounded-4xl py-2 pl-6 bg-zinc-800 text-2xl hover:cursor-pointer hover:text-black hover:bg-white transition duration-150 ease-in-out">
+          <button className="group flex items-center gap-4 mt-2 rounded-4xl py-2 pl-6 bg-zinc-800 text-2xl transition-colors hover:cursor-pointer hover:text-black hover:bg-white duration-150 ease-in-out">
             <p>Let's Go</p>
-            <div className="text-xl p-2 rounded-4xl bg-white text-black self-end">
+            <div className="text-xl p-2 rounded-4xl bg-white text-black self-end heroButton">
               <FaArrowRightLong />
             </div>
           </button>
